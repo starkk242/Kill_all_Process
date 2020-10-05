1a1b05227c4e137aa375368eed9688213973803c
Bimal Singh <bimalsingh983@gmail.com>
2020-10-05 18:30:03 +0530

    Update kill.py

--- kill.py
+++ kill.py
@@ -1,31 +1,20 @@
 def kill_all():
 	for proc in psutil.process_iter(['pid', 'name', 'username']):
 	    try:
-	        if((proc.info['username'] == None)):
+	        if((proc.info['username'] == None)): #If username is None then process will not be Killed
 	            print('skipped :',proc.info['pid'])
-	        elif((proc.info['name'] == None)):
+	        elif((proc.info['name'] == None)):# Ignore this one
 	            print('skipped :',proc.info['pid'])
-	        elif('WindowsTerminal.exe' in proc.info['name']):
-	            print('skipped 1 :',proc.info['pid'])
-	        elif('Qt5gui.exe' in proc.info['name']):
-	            print('skipped 1 :',proc.info['pid'])
-	        elif('conhost.exe' in proc.info['name']):
+	        elif('conhost.exe' in proc.info['name']):# Important file for windows
 	            print('skipped 2 :',proc.info['pid'])
-	        elif('msedge.exe' in proc.info['name']):
+	        elif('python.exe' in proc.info['name']):# Python is Important
 	            print('skipped 2 :',proc.info['pid'])
-	        elif('python.exe' in proc.info['name']):
-	            print('skipped 2 :',proc.info['pid'])
-	        elif('explorer.exe' in proc.info['name']):
-	            print('skipped 2 :',proc.info['pid'])
-	        elif('cmd.exe' in proc.info['name']):
-	            print('skipped 2 :',proc.info['pid'])
-	        elif('sublime_text.exe' in proc.info['name']):
+	        elif('cmd.exe' in proc.info['name']):# Running this script on what?
 	            print('skipped 2 :',proc.info['pid'])	            
-	        elif(psutil.users()[0][0] not in proc.info['username']):
+	        elif(psutil.users()[0][0] not in proc.info['username']):# Ignore this one too
 	            print('skipped :',proc.info['pid'])
 	        else:
 	            print(proc.info)
-	            #x=input('else : ')
 	            print('killed')
 	            proc.kill()
 	    except Exception as e:
